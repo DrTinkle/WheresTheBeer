97f039863e055d65c2c40d8bf285696f1b4af1da
DrTinkle <roykoljonen@hotmail.com>
2024-09-24 11:13:37 +0300

    Adjusted margin for mobile

--- WheresTheBeer.Client/obj/Debug/net8.0/WheresTheBeer.Client.AssemblyInfo.cs
+++ WheresTheBeer.Client/obj/Debug/net8.0/WheresTheBeer.Client.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WheresTheBeer.Client")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8a1cc2531b19dbb275ea907b122b310aaeae95ad")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8bcea107659f6a647fff41268621b92ee2690525")]
 [assembly: System.Reflection.AssemblyProductAttribute("WheresTheBeer.Client")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WheresTheBeer.Client")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
